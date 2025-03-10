0c109bb077ed9ee8a66d9b08ec147ba7f8556c98
yfturzyjrtx <156646603+yfturzyjrtx@users.noreply.github.com>
2025-03-10 15:52:23 +0000

    I add a paiement page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './PAGES/Home';
 import "./App.css"
 import Profil from './PAGES/profil';
+import Ap from './components/paiement';
 
 function App() {
   return (<>
     { /*<Home /> */ }
     { /*<Formpc /> */ }
     
-    <Profil/>
+    <Ap/>
     </>
   );
 }
