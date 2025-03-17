b4c7c3279ec3a1b68bcbb68d9bbc0ec9cfb53fd9
yfturzyjrtx <156646603+yfturzyjrtx@users.noreply.github.com>
2025-03-17 15:38:23 +0000

    i

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import Livraison from './PAGES/livraison';
 import Paiement from './PAGES/paiement';
 import Aboutt from './PAGES/about';
 import Solve from './PAGES/solve';
+import Home from "../src/PAGES/Home"
+import Profil from './PAGES/profil';
 
 
 
 function App() {
   return (<>
-    { /*<Home />
+    { /*
+    
     <ProductCard/>
     <Livraison />
     <Paiement/>
-    <Aboutt /> */ }
-    
+    <Aboutt /> 
     <Solve />
+    <Profil/>
+    */ }
     
+    
+    
+    <Home />
     
     </>
   );
--- src/PAGES/Home.js
+++ src/PAGES/Home.js
@@ -92,14 +92,6 @@ export default function Home(){
 
 
 
-
-
-
-
-
-
-
-
-            </>
+    </>
     );
 }
